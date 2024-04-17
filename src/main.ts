163543b104a33b9421658879857663188e85d922
Rex Berry <59031902+RexBerry@users.noreply.github.com>
2024-04-17 16:29:52 -0500

    Tweak bloom

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ function main(): void {
         dynamicRange: 5.0,
         isAntialiasingEnabled: false,
         verticalFov: 80.0,
-        bloomThresholdRatio: 1.0,
+        bloomThresholdRatio: 1.5,
         bloomRadius: 6.0,
     }
 
